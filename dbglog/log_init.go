57d1f506be3acf0bfa330317d17cecd87c0efd87
hz <hedzrz@gmail.com>
2022-03-22 16:45:08 +0800

    improved dbglog initializing codes

--- dbglog/log_init.go
+++ dbglog/log_init.go
@@ -1,7 +1,16 @@
 package dbglog
 
-import "log"
+import (
+	log2 "github.com/hedzr/log"
+	"log"
+)
 
 func init() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+
+	// enable debug level for hedzr/log and log.
+	// - make log.Debugf(...) printing the text onto console;
+	// - make log.VDebugf(...) printing the text onto console
+	//   if build tag 'verbose' present;
+	log2.SetLevel(log2.DebugLevel)
 }
