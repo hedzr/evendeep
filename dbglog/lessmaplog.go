4cb9f08b76445211d6c02207b2abe5a8cd8ab813
hz <hedzrz@gmail.com>
2023-03-04 23:36:37 +0800

    doc dbglog.DisableLog()

--- dbglog/lessmaplog.go
+++ dbglog/lessmaplog.go
@@ -5,5 +5,17 @@ package dbglog
 
 const MoreMapLog = true
 
+// DisableLog can be used to disable dbglog.Log at runtime.
+//
+// It detects and prevent log output if buildtag 'moremaplog' present.
+//
+// To query the active state by calling ChildLogEnabled.
+//
+// The best practise for DisableLog is:
+//
+//    defer dbglog.DisableLog()()
+//    evendeep.CopyTo(...) // the verbose logging will be prevent even if buildtag 'verbose' defined.
+//
+// If you want to enable/disable dbglog.Log manually, try SetLogEnabled and SetLogDisabled.
 func DisableLog() func()    { return func() {} }
 func ChildLogEnabled() bool { return true }
--- dbglog/lessmaplog_false.go
+++ dbglog/lessmaplog_false.go
@@ -7,5 +7,16 @@ const MoreMapLog = false
 
 var logValid = true
 
+// DisableLog can be used to disable dbglog.Log at runtime.
+//
+// It detects and prevent log output if buildtag 'moremaplog' present.
+//
+// To query the active state by calling ChildLogEnabled.
+//
+// The best practise for DisableLog is:
+//
+//    defer dbglog.DisableLog()()
+//    evendeep.CopyTo(...) // the verbose logging will be prevent even if buildtag 'verbose' defined.
+//
 func DisableLog() func()    { var sav = logValid; logValid = MoreMapLog; return func() { logValid = sav } }
 func ChildLogEnabled() bool { return logValid }
