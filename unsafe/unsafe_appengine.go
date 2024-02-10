58af89f487411834d240b86799dd5cecffd3bba3
hz <hedzrz@gmail.com>
2024-02-10 09:47:14 +0800

    make unsafe helpers public

---
+++ unsafe/unsafe_appengine.go
@@ -0,0 +1,21 @@
+//go:build appengine || disableunsafe
+// +build appengine disableunsafe
+
+package unsafe
+
+import "reflect"
+
+const (
+	// UnsafeDisabled is a build-time constant which specifies whether or
+	// not access to the unsafe package is available.
+	UnsafeDisabled = true
+)
+
+// UnsafeReflectValue typically converts the passed reflect.Value into a one
+// that bypasses the typical safety restrictions preventing access to
+// unaddressable and unexported data.  However, doing this relies on access to
+// the unsafe package.  This is a stub version which simply returns the passed
+// reflect.Value when the unsafe package is not available.
+func UnsafeReflectValue(v reflect.Value) reflect.Value {
+	return v
+}
