7ffa405c4b08940f3add39f5d6d59f7256633653
hz <hedzrz@gmail.com>
2024-02-17 12:12:41 +0800

    added TrimQuotes, StripQuotes

--- internal/tool/strings.go
+++ internal/tool/strings.go
@@ -7,3 +7,38 @@ type String string
 func (s String) Split(sep string) []string {
 	return strings.Split(string(s), sep)
 }
+
+// StripQuotes strips first and last quote char (double quote or single quote).
+func StripQuotes(s string) string { return trimQuotes(s) }
+
+// TrimQuotes strips first and last quote char (double quote or single quote).
+func TrimQuotes(s string) string { return trimQuotes(s) }
+
+// func trimQuotes(s string) string {
+// 	if len(s) >= 2 {
+// 		if c := s[len(s)-1]; s[0] == c && (c == '"' || c == '\'') {
+// 			return s[1 : len(s)-1]
+// 		}
+// 	}
+// 	return s
+// }
+
+func trimQuotes(s string) string {
+	switch {
+	case s[0] == '\'':
+		if s[len(s)-1] == '\'' {
+			return s[1 : len(s)-1]
+		}
+		return s[1:]
+	case s[0] == '"':
+		if s[len(s)-1] == '"' {
+			return s[1 : len(s)-1]
+		}
+		return s[1:]
+	case s[len(s)-1] == '\'':
+		return s[0 : len(s)-1]
+	case s[len(s)-1] == '"':
+		return s[0 : len(s)-1]
+	}
+	return s
+}
